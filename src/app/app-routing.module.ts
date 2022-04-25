0639294820476af6c1251e5ee8cb1626a97120c3
KibaeKim <kibaekim@yahoo.ca>
2022-04-24 21:47:54 -0700

    Initialized HomePage, SignupPage, LoginPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signup', loadChildren: () => import('./pages/signup-page/signup-page.module').then(m => m.SignupPageModule) },
+  { path: 'login', loadChildren: () => import('./pages/login-page/login-page.module').then(m => m.LoginPageModule) },
+  { path: '', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
